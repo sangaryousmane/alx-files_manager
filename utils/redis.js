967d7220aec2581f1e5f51b27b829d305af5255c
sangaryousmane <sangaryousmane@gmail.com>
2024-06-19 14:01:48 -0700

    Add comments to all the functions

--- utils/redis.js
+++ utils/redis.js
@@ -13,19 +13,37 @@ class RedisClient {
     this.client.on('connect', () => console.log('Redis Client has been connected'));
   }
 
+  /**
+   * Check if the Redis server is connected or active
+   * @return {boolean} true if alive, false if not
+   */
   isAlive () {
     return this.client.connected;
   }
 
+  /**
+   * Get the value given the key
+   * @key {string} the key to search for
+   * @return {string} the value based on the key
+   */
   async get (key) {
     const value = await this.getAsync(key);
     return value;
   }
 
+  /**
+   * Set a new key and value record in Redis
+   * @key {string} a key to set
+   * @value {string} the value to add
+   */
   async set (key, value, duration) {
     this.client.setex(key, duration, value);
   }
 
+  /**
+   * Delete a key from redis db
+   * @key {string} the key to delete
+   */
   async del (key) {
     this.client.del(key);
   }
