8f529ef7227b42ec997bbe104ab4b7306e12a255
sangaryousmane <sangaryousmane@gmail.com>
2024-06-19 02:49:48 -0700

    Start redis server to test tasks

--- utils/redis.js
+++ utils/redis.js
@@ -1,43 +1,42 @@
 /* Make a redis client */
 
-const redis = require("redis")
+const redis = require('redis');
 
 class RedisClient {
+  constructor () {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.error('Redis Client Error:', err));
+  }
 
-	constructor() {
-		this.client = redis.createClient();
-		this.client.on('error', (err) => console.error('Redis Client Error:', err));
-	}
+  isAlive () {
+    return this.client.connected;
+  }
 
-	isAlive() {
-		return this.client.connected;
-	}
+  async get (key) {
+    try {
+      const value = await this.client.get(key);
+      return value;
+    } catch (err) {
+      console.error(`Error getting key: ${key}, ${err}`);
+      return null;
+    }
+  }
 
-	async get(key) {
-		try {
-			const value = await this.client.get(key);
-			return value;
-		} catch ( err ) {
-			console.error(`Error getting key: ${key}, ${err}`)
-			return null;_
-		}
-	}
+  async set (key, value, duration) {
+    try {
+      await this.client.set(key, value, 'EX', duration);
+    } catch (err) {
+      console.error(`Error setting key: ${key}, ${err}`);
+    }
+  }
 
-	async set(key, value, duration) {
-		try {
-			await this.client.set(key, value, "EX", duration);
-		} catch ( err ) {
-			console.error(`Error setting key: ${key}, ${error}`)
-		}
-	}
-
-	async del (key) {
-		try {
-			await this.client.del(key);
-		} catch ( err ) {
-			console.error(`Error deleting key ${key}, ${err}`)
-		}
-	}
+  async del (key) {
+    try {
+      await this.client.del(key);
+    } catch (err) {
+      console.error(`Error deleting key ${key}, ${err}`);
+    }
+  }
 }
-const redisClient = new RedisClient()
+const redisClient = new RedisClient();
 module.exports = redisClient;
