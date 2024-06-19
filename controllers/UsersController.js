1d2710a78ba18021d15bfcd887a633c91489ee99
sangaryousmane <sangaryousmane@gmail.com>
2024-06-19 15:08:45 -0700

    Modify user controller and add a new authentication controller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,12 +2,11 @@ import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
 import Queue from 'bull';
 import dbClient from '../utils/db';
-import userUtils from '../utils/user';
-
+import userDetail from '../utils/userDetail';
 const userQueue = new Queue('userQueue');
 
-class class UsersController {
-  static async postNew(request, response) {
+class UsersController {
+  static async postNew (request, response) {
     const { email, password } = request.body;
 
     if (!email) return response.status(400).send({ error: 'Missing email' });
@@ -24,7 +23,7 @@ class class UsersController {
     try {
       result = await dbClient.usersCollection.insertOne({
         email,
-        password: sha1Password,
+        password: sha1Password
       });
     } catch (err) {
       await userQueue.add({});
@@ -33,13 +32,30 @@ class class UsersController {
 
     const user = {
       id: result.insertedId,
-      email,
+      email
     };
 
     await userQueue.add({
-      userId: result.insertedId.toString(),
+      userId: result.insertedId.toString()
     });
 
     return response.status(201).send(user);
- }
+  }
+
+  static async getMe (req, res) {
+    const { ID } = await userDetail.getUserIdAndKey(req);
+
+    const user = await userDetail.getUser({
+      _id: ObjectId(ID)
+    });
+
+    if (!user) return res.status(401).send({ error: 'Unauthorized' });
+    const userData = { id: user._id, ...user };
+    delete userData._id;
+    delete userData.password;
+
+    return res.status(200).send(userData);
+  }
 }
+
+export default new UsersController();
