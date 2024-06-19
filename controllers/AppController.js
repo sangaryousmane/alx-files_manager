89efdf8b2712fda7cb0ae0b08b160cac457a6cbb
sangaryousmane <sangaryousmane@gmail.com>
2024-06-19 03:23:16 -0700

    Add all route controllers

---
+++ controllers/AppController.js
@@ -0,0 +1,23 @@
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
+
+class AppController {
+  static async getStatus (req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive()
+    });
+  }
+
+  static async getStats (req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+
+    res.status(200).json({
+      users: usersCount,
+      files: filesCount
+    });
+  }
+}
+
+export default AppController;
