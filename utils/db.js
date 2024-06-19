789eb8a8eb4f0603b62d13f428d08e7c5649e4dd
sangaryousmane <sangaryousmane@gmail.com>
2024-06-19 15:25:33 -0700

    Modify and add new files to the utility files

--- utils/db.js
+++ utils/db.js
@@ -1,58 +1,55 @@
 import { MongoClient } from 'mongodb';
 
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
 /**
- * A DB connection service for mongo client
+ * Class for performing operations with Mongo service
  */
 class DBClient {
-  constructor () {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-
-    const url = `mongodb://${host}:${port}/${database}`;
-
-    this.client = new MongoClient(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-
-    this.client.connect((err) => {
-      if (err) {
-        console.error(`MongoDB connection error: ${err}`);
+  constructor() {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        // console.log('Connected successfully to server');
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
       } else {
-        console.log('Connected to MongoDB');
+        console.log(err.message);
+        this.db = false;
       }
     });
   }
 
-  isAlive () {
-    return !!this.client && this.client.isConnected();
+  /**
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
+   */
+  isAlive() {
+    return Boolean(this.db);
   }
 
-  async nbUsers () {
-    try {
-      const db = this.client.db();
-      const usersCollection = db.collection('users');
-      const count = await usersCollection.countDocuments();
-      return count;
-    } catch (err) {
-      console.error('Error counting users:', err);
-      return -1;
-    }
+  /**
+   * Returns the number of documents in the collection users
+   * @return {number} amount of users
+   */
+  async nbUsers() {
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
   }
 
-  async nbFiles () {
-    try {
-      const db = this.client.db();
-      const filesCollection = db.collection('files');
-      const count = await filesCollection.countDocuments();
-      return count;
-    } catch (err) {
-      console.error('Error counting files:', err);
-      return -1;
-    }
+  /**
+   * Returns the number of documents in the collection files
+   * @return {number} amount of files
+   */
+  async nbFiles() {
+    const numberOfFiles = this.filesCollection.countDocuments();
+    return numberOfFiles;
   }
 }
 
 const dbClient = new DBClient();
+
 export default dbClient;
--- utils/redis.js
+++ utils/redis.js
@@ -2,51 +2,61 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 /**
- * This class is use performing operations with Redis service
+ * Class for performing operations with Redis service
  */
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = redis.createClient();
     this.getAsync = promisify(this.client.get).bind(this.client);
-    this.client.on('error', (err) => console.error('Redis Client Error:', err.message));
 
-    this.client.on('connect', () => console.log('Redis Client has been connected'));
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+
+    this.client.on('connect', () => {
+      // console.log('Redis client connected to the server');
+    });
   }
 
   /**
-   * Check if the Redis server is connected or active
-   * @return {boolean} true if alive, false if not
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
    */
-  isAlive () {
+  isAlive() {
     return this.client.connected;
   }
 
   /**
-   * Get the value given the key
-   * @key {string} the key to search for
-   * @return {string} the value based on the key
+   * gets value corresponding to key in redis
+   * @key {string} key to search for in redis
+   * @return {string}  value of key
    */
-  async get (key) {
+  async get(key) {
     const value = await this.getAsync(key);
     return value;
   }
 
   /**
-   * Set a new key and value record in Redis
-   * @key {string} a key to set
-   * @value {string} the value to add
+   * Creates a new key in redis with a specific TTL
+   * @key {string} key to be saved in redis
+   * @value {string} value to be asigned to key
+   * @duration {number} TTL of key
+   * @return {undefined}  No return
    */
-  async set (key, value, duration) {
+  async set(key, value, duration) {
     this.client.setex(key, duration, value);
   }
 
   /**
-   * Delete a key from redis db
-   * @key {string} the key to delete
+   * Deletes key in redis service
+   * @key {string} key to be deleted
+   * @return {undefined}  No return
    */
-  async del (key) {
+  async del(key) {
     this.client.del(key);
   }
 }
+
 const redisClient = new RedisClient();
+
 export default redisClient;
