d253ef3e0aa00d55fa1fa9c177e51d31c42e999c
sangaryousmane <sangaryousmane@gmail.com>
2024-06-21 11:19:39 -0700

    Fix eslinting error

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,19 +2,18 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  
- static getStatus(request, response) {
+  static getStatus (request, response) {
     const status = {
       redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
+      db: dbClient.isAlive()
     };
     response.status(200).send(status);
   }
 
-  static async getStats(request, response) {
+  static async getStats (request, response) {
     const stats = {
       users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
+      files: await dbClient.nbFiles()
     };
     response.status(200).send(stats);
   }
